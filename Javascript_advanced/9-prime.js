6412efc05ade7d7e120ab9d5f79955fd1379e386
Franklin11111 <abdulovnail6@gmail.com>
2025-07-22 20:22:32 +0400

    Implemented Prime numbers & timing execution

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,27 @@
+function countPrimeNumbers() {
+    let start = 2;
+    let end = 100;
+    function isPrime(num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const primes = [];
+    for (let i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+    return primes.length;
+}
+const t0 = performance.now();
+console.log(countPrimeNumbers());
+const t1 = performance.now();
+const timing = t1 - t0;
+console.log(`Execution time of printing countPrimeNumbers was ${timing} milliseconds`);
