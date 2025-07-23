5f2d828d27cb9699d9cfb5a289f5721a5e75cde4
Franklin11111 <abdulovnail6@gmail.com>
2025-07-23 11:26:36 +0400

    Implemented 'Simple callback'

---
+++ Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,21 @@
+function createElement(data) {
+    const pElement = document.createElement("p");
+    pElement.innerHTML = data;
+    document.body.append(pElement);
+}
+
+function queryWikipedia(callback) {
+    const xhr = new XMLHttpRequest();
+    const url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*"
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = this.responseText;
+            callback(data);
+        }
+    };
+
+    xhr.send();
+}
+
+queryWikipedia(createElement);
