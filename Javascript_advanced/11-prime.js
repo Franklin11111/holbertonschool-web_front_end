364946118ae2aafc8b41ce66109ae5da90712b65
Franklin11111 <abdulovnail6@gmail.com>
2025-07-23 09:35:30 +0400

    Implemented 'Execution stack & timing execution' and 'Changing stack order using setTimeout'

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,34 @@
+function countPrimeNumbers() {
+    let start = 2;
+    let end = 100;
+    function isPrime(num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const primes = [];
+    for (let i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+    return primes.length;
+}
+const t0 = performance.now();
+
+for (let i = 0; i < 100; i++) {
+    setTimeout(() => {
+        console.log(countPrimeNumbers());
+    }, 0)
+};
+
+const t1 = performance.now();
+
+const timing = t1 - t0;
+console.log(`Execution time of calculating prime numbers 100 times was ${timing} milliseconds`);
